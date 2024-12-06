3b0ce3c24fbd9c724f1fa8cee33f112e9105faca
nikita kropinoff <132770307+nkropinoff@users.noreply.github.com>
2024-12-06 15:12:37 +0300

    Update Main.java
    second commit

--- src/main/java/org/example/infexample/lab10/Main.java
+++ src/main/java/org/example/infexample/lab10/Main.java
@@ -2,6 +2,6 @@ package org.example.infexample.lab10;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello 400!");
+        System.out.println("Hello 400000!");
     }
 }
